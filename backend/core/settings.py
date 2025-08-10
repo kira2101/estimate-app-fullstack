1f609aea7b4afe385b28d0d27022b1249d4cc2e4
Kir <kira2101@gmail.com>
2025-08-10 12:05:30 +0300

    Исправлена ошибка CORS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -61,10 +61,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+
 
 ROOT_URLCONF = 'core.urls'
 
